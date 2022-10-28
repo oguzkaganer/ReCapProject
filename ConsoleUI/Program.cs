039b878063cf518ba52b73f239bca9693512e128
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-10-28 18:38:34 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            var carList = carManager.GetAll();
+
+            foreach (var car in carList)
+            {
+                Console.WriteLine(car.BrandId);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1, ModelYear=2022 , DailyPrice=950,Description="Otomatik,Elektrikli"},
+                new Car{Id=2,BrandId=1,ColorId=1, ModelYear=2019 , DailyPrice=500,Description="Manuel,Benzin"},
+                new Car{Id=3,BrandId=2,ColorId=2, ModelYear=2020 , DailyPrice=600,Description="Otomatik,Dizel"},
+                new Car{Id=4,BrandId=2,ColorId=1, ModelYear=2020 , DailyPrice=600,Description="Otomatik,Dizel"},
+                new Car{Id=5,BrandId=3,ColorId=3, ModelYear=2021 , DailyPrice=750,Description="Otomatik,Benzin"},
+            };
+        }
+
+
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int Id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == Id);
+        }
+    }
+}
