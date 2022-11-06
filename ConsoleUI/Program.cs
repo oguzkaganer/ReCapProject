5eaf032d9e4ea316a82fefcb09d604e9db5e45cc
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-11-06 14:47:32 +0300

    Car, Brand ve Color için tüm CRUD operasyonları hazır hale getirildi.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,20 +10,42 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             //CarTest();
-            CarManager carManager = new CarManager(new EfCarDal());
 
-            Car car1 = new Car() {Id=1, BrandId = 1, ColorId = 1, ModelYear = 2022, DailyPrice = 650, Description = "Manuel" };
+            //BrandTest();
 
-            carManager.Update(car1);
+            //ColorTest();
 
-            int i=1;
-            foreach (var car in carManager.GetCarDetails())
-            {
-                Console.WriteLine(i+". "+car.Id+" - "+car.BrandName+" - "+car.ColorName+" - "+car.DailyPrice);
-                i++;
-            }
-           
+
+
             Console.ReadLine();
+        }
+
+        private static void ColorTest()
+        {
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+
+            Color color1 = new Color() { Name = "Orange" };
+
+            colorManager.Add(color1);
+
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.Id + " - " + color.Name);
+            }
+        }
+
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+
+            Brand brand1 = new Brand() { Name = "Alfa Romeo" };
+
+            brandManager.Add(brand1);
+
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.Id + " - " + brand.Name);
+            }
         }
 
         private static void CarTest()
@@ -47,6 +69,16 @@ namespace ConsoleUI
             foreach (var car in carManager.GetCarsByColorId(1))
             {
                 Console.WriteLine(car.BrandId);
+            }
+
+            Car car2 = new Car() { Id = 1, BrandId = 1, ColorId = 1, ModelYear = 2022, DailyPrice = 650, Description = "Manuel" };
+
+            carManager.Update(car2);
+
+            foreach (var car in carManager.GetCarDetails())
+            {
+                Console.WriteLine(car.Id + " - " + car.BrandName + " - " + car.ColorName + " - " + car.DailyPrice);
+
             }
         }
     }
