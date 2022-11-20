f4a974bdd35ab54bb4089c21cf57dea1143b532d
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-11-20 22:48:36 +0300

    User,Customer ve Rental entityleri eklendi ve temel operasyonları yapıldı.

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using Core.Utilities;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll());
+        }
+
+        public IResult Add(Rental rental)
+        {
+
+            if (rental.ReturnDate == null)
+            {
+                return new ErrorResult("Bu arabayı kiralayamazsınız.");
+            }
+
+            _rentalDal.Add(rental);
+            return new SuccessResult("Kiralama işlemi başarılı.");
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult();
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,14 +10,71 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             //CarTest();
-
-            BrandTest();
-
-            ColorTest();
-
-
+            //BrandTest();
+            //ColorTest();
+            //UserTest();
+            //CustomerTest();
+            //RentalTest();
 
             Console.ReadLine();
+        }
+
+        private static void RentalTest()
+        {
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+
+            Rental rental1 = new Rental()
+            {
+                CarId = 1,
+                CustomerId = 1,
+                RentDate = DateTime.Now,
+                ReturnDate = null
+                //ReturnDate = DateTime.Now.AddDays(1)
+            };
+
+            Console.WriteLine(rentalManager.Add(rental1).Message);
+        }
+
+        private static void CustomerTest()
+        {
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+
+            Customer customer1 = new Customer()
+            {
+                UserId = 1,
+                CompanyName = "SmartPro"
+            };
+
+            //customerManager.Add(customer1);
+
+            foreach (var customer in customerManager.GetAll().Data)
+            {
+                Console.WriteLine(customer.CompanyName);
+            }
+        }
+
+        private static void UserTest()
+        {
+            UserManager userManager = new UserManager(new EfUserDal());
+
+            User user1 = new User()
+            {
+                Id = 1,
+                FirstName = "Oğuz Kağan",
+                LastName = "ER",
+                Email = "oguzkagan@gmail.com",
+                Password = "1234"
+            };
+
+            //userManager.Add(user1);
+            //userManager.Update(user1);
+
+            foreach (var user in userManager.GetAll().Data)
+            {
+                Console.WriteLine(user.FirstName);
+            }
+
+            Console.WriteLine(userManager.GetAll().Success);
         }
 
         private static void ColorTest()
