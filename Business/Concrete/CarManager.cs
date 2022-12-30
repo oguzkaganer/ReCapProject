568509915f16d23f8c038f233726c3ebf0b8469a
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-12-30 22:09:56 +0300

    Added FluentValidation support for validations

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation.FluentValidation;
 using Core.Utilities;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -12,64 +14,58 @@ using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
-    public class CarManager : ICarService
-    {
-        ICarDal _carDal;
+	public class CarManager : ICarService
+	{
+		ICarDal _carDal;
 
-        public CarManager(ICarDal carDal)
-        {
-            _carDal = carDal;
-        }
+		public CarManager(ICarDal carDal)
+		{
+			_carDal = carDal;
+		}
 
-        public IDataResult<List<Car>> GetAll()
-        {
-            return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.CarListed);
-        }
+		public IDataResult<List<Car>> GetAll()
+		{
+			return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.CarListed);
+		}
 
-        public IDataResult<Car> Get(int id)
-        {
-            return new SuccessDataResult<Car>(_carDal.Get(c => c.Id == id));
-        }
+		public IDataResult<Car> Get(int id)
+		{
+			return new SuccessDataResult<Car>(_carDal.Get(c => c.Id == id));
+		}
 
-        public IDataResult<List<Car>> GetCarsByBrandId(int id)
-        {
-            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == id));
-        }
+		public IDataResult<List<Car>> GetCarsByBrandId(int id)
+		{
+			return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == id));
+		}
 
-        public IDataResult<List<Car>> GetCarsByColorId(int id)
-        {
-            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == id));
-        }
+		public IDataResult<List<Car>> GetCarsByColorId(int id)
+		{
+			return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == id));
+		}
 
-        public IResult Add(Car car)
-        {
-            if ((car.DailyPrice > 0))
-            {
-                _carDal.Add(car);
-                return new SuccessResult(Messages.CarAdded);
-            }
-            else
-            {
-                Console.WriteLine("Günlük fiyat 0'dan küçük olamaz");
-                return new ErrorResult(Messages.InvalidOperation);
-            }
-        }
+		public IResult Add(Car car)
+		{
+			ValidationTool.Validate(new CarValidator(),car);
 
-        public IResult Update(Car car)
-        {
-            _carDal.Update(car);
-            return new SuccessResult(Messages.CarUpdated);
-        }
+			_carDal.Add(car);
+			return new SuccessResult(Messages.CarAdded);
+		}
 
-        public IResult Delete(Car car)
-        {
-            _carDal.Delete(car);
-            return new SuccessResult(Messages.CarDeleted);
-        }
+		public IResult Update(Car car)
+		{
+			_carDal.Update(car);
+			return new SuccessResult(Messages.CarUpdated);
+		}
 
-        public IDataResult<List<CarDetailDto>> GetCarDetails()
-        {
-            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
-        }
-    }
+		public IResult Delete(Car car)
+		{
+			_carDal.Delete(car);
+			return new SuccessResult(Messages.CarDeleted);
+		}
+
+		public IDataResult<List<CarDetailDto>> GetCarDetails()
+		{
+			return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
+		}
+	}
 }
